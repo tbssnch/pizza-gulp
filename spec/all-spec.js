cf686079b40407a3835118fc098c01917b8773c5
mikey pinaud, Sailor Winkelman and tyler bussanich <mikey@>
2018-01-03 17:19:30 -0800

    refactor testing

--- spec/all-spec.js
+++ spec/all-spec.js
@@ -1,19 +1,36 @@
 var Contact = require('./../js/contact.js').contactModule;
 var Pizza = require('./../js/pizza.js').pizzaModule;
 
-describe('fullName', function() {
-  it('should test whether user first and last name create full name', function(){
-    var newContact = new Contact("John", "Sawiris");
-    expect(newContact.fullName()).toEqual("John Sawiris");
+describe('contact', function() {
+  var reusableContact;
+
+  beforeEach(function() {
+    reusableContact = new Contact("John", "Sawiris", "1234 SW Washington", "Portland", "Oregon", "97214");
   });
+
+  it('should test whether user first and last name create full name', function(){
+    expect(reusableContact.fullName()).toEqual("John Sawiris");
+  });
+
+  it('should test whether user street, city, and state create the full address', function(){
+    expect(reusableContact.fullAddress()).toEqual("1234 SW Washington Portland, Oregon 97214");
+  });
+
 });
 
-describe('fullAddress', function() {
-  it('should test whether user street, city, and state create the full address', function(){
-    var newAddress = new Contact("Tyler", "Bu", "1234 SW Washington", "Portland", "Oregon", "97214");
-    expect(newAddress.fullAddress()).toEqual("1234 SW Washington Portland, Oregon 97214");
-  });
-});
+// describe('fullName', function() {
+//   it('should test whether user first and last name create full name', function(){
+//     var newContact = new Contact("John", "Sawiris");
+//     expect(newContact.fullName()).toEqual("John Sawiris");
+//   });
+// });
+
+// describe('fullAddress', function() {
+//   it('should test whether user street, city, and state create the full address', function(){
+//     var newAddress = new Contact("Tyler", "Bu", "1234 SW Washington", "Portland", "Oregon", "97214");
+//     expect(newAddress.fullAddress()).toEqual("1234 SW Washington Portland, Oregon 97214");
+//   });
+// });
 
 describe('pizzaSize', function() {
   it('should test if pizza small size returns 14', function(){
